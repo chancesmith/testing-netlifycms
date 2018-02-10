689d256559e29851215a4e8faa9f15479cc0a337
Chance Smith <chancesmithb@gmail.com>
2018-02-10 03:14:49 -0600

    adds in login script

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -25,6 +25,17 @@ const TemplateWrapper = ({ children }) => (
     >
       {children()}
     </div>
+    <script>
+      if (window.netlifyIdentity) {
+        window.netlifyIdentity.on("init", user => {
+          if (!user) {
+            window.netlifyIdentity.on("login", () => {
+              document.location.href = "/admin/";
+            });
+          }
+        });
+      }
+    </script>
   </div>
 )
 
